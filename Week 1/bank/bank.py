26ee6a0cb0c8ffb5b55de8dfcd274d880410d314
taahahussainkhan <taaha128@gmail.com>
2023-04-21 19:32:24 +0500

    Week 1 deep and bank added

---
+++ Week 1/bank/bank.py
@@ -0,0 +1,7 @@
+greet=input("Greeting: ")
+if greet == 'Hello' or greet == 'Hello, Newman':
+    print('$0')
+elif greet == 'How you doing?':
+    print('$20')
+elif greet == "What's happening?":
+    print('$100')
