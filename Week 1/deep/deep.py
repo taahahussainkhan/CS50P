eb051998079e56c508139d6c294b8905bdeac64a
taahahussainkhan <taaha128@gmail.com>
2023-04-21 21:01:00 +0500

    Interpreter

---
+++ Week 1/deep/deep.py
@@ -0,0 +1,5 @@
+answer=input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower().strip()
+if answer == '42' or answer =='forty-two' or answer == 'forty two':
+    print('Yes')
+else:
+    print('No')
